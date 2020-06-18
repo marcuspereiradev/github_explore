7cb78041cb79e712841794b6e051ea1cc593c3e7
Marcus Pereira <marcus.work@hotmail.com>
2020-06-18 15:19:56 -0300

    Adding styles with stayled components and assets

---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #fff url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: Oswald, sans-serif;
+    font-size: 1em;
+  }
+
+  #root {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
