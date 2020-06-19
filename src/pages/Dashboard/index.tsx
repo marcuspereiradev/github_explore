41978878d6e852f2f69e50e44068016e559f9553
Marcus Pereira <marcus.work@hotmail.com>
2020-06-19 14:30:05 -0300

    Creating Dashboard layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => {
-  return <Title>Explore repositories on Github</Title>;
-};
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github explorer" />
+    <Title>Explore Repositories on Github</Title>
+
+    <Form>
+      <input type="text" placeholder="Type the repository name" />
+      <button type="submit">Search</button>
+    </Form>
+
+    <Repositories>
+      <a href="testjusttostoperror">
+        <img
+          src="https://avatars3.githubusercontent.com/u/30603440?s=460&u=f64d173bbbf1d830200422971ae84e473b499e50&v=4"
+          alt="Marcus Pereira"
+        />
+        <div>
+          <strong>marcuspereiradev/github_explorer</strong>
+          <p>SPA with React using Github API to explore users repositories</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,4 +3,86 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   color: #3a3a3a;
   font-size: 3em;
+  line-height: 56px;
+  margin-top: 80px;
+  max-width: 450px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+
+  input {
+    background: #363636;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #e5e5e5;
+    height: 70px;
+    flex: 1;
+    padding: 0 24px;
+
+    &::placeholder {
+      color: #a5a5a5;
+    }
+  }
+
+  button {
+    background: #f8a31a;
+    border: 0;
+    border-radius: 0 5px 5px 0;
+    font-weight: bold;
+    transition: 0.2s;
+    width: 210px;
+
+    &:hover {
+      background: #e29314;
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+
+  a {
+    align-items: center;
+    background: #363636;
+    display: flex;
+    padding: 24px;
+    text-decoration: none;
+    transition: 0.3s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      border-radius: 50%;
+      height: 64px;
+      width: 64px;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 16px;
+
+      strong {
+        color: #a5a5a5a5;
+        font-size: 1.25em;
+      }
+
+      p {
+        color: #e5e5e5e5;
+        font-size: 1.125em;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      color: #e5e5e5e5;
+    }
+  }
 `;
